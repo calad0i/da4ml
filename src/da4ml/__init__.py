ba9e196fbb273dbdcfb117db905fd9a79ee10873
Chang Sun <chsun@cern.ch>
2024-10-22 03:21:02 +0000

    expose apis

---
+++ src/da4ml/__init__.py
@@ -0,0 +1,5 @@
+from .cmvm.cmvm import compile_kernel
+from .cmvm.utils import DAState, Score, OpCode
+from .cmvm.codegen import PyCodegenBackend
+from .cmvm.graph_compile import graph_compile_states
+from .cmvm.api import fn_from_kernel, cost
