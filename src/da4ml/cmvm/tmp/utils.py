6c75f8ee2bbbaf3a0ef7ee4bacae65623d0b3b32
Chang Sun <chsun@cern.ch>
2025-04-22 17:54:57 -0700

    cleanup obsolete files

--- src/da4ml/cmvm/tmp/utils.py
+++
@@ -1,5 +0,0 @@
-from collections import namedtuple
-
-DAState = namedtuple('DAState', ['csd', 'variables', 'op_codes', 'pairs', 'score', 'kernel'])
-Score = namedtuple('Score', ['potential', 'realized', 'lost', 'value'])
-OpCode = namedtuple('OPCode', ['pos0', 'pos1', 'shift0', 'shift1', 'sign0', 'sign1'])
