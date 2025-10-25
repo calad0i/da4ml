210bd1ad4c7ac120a9c966cab20983dc51cd43ae
Chang Sun <chsun@cern.ch>
2025-10-25 15:57:39 -0700

    fix(rtl): vhdl verification makefile w/o memfile

--- src/da4ml/codegen/rtl/common_source/build_binder.mk
+++ src/da4ml/codegen/rtl/common_source/build_binder.mk
@@ -12,7 +12,7 @@ VERILATOR_FLAGS ?=
 ../src/$(VM_PREFIX).v: $(wildcard ../src/$(VM_PREFIX).vhd) $(wildcard ../src/$(VM_PREFIX)_stage*.vhd)
 # vhdl specific - convert to verilog first for verilating
 	mkdir -p obj_dir
-	cp ../src/memfiles/* ./
+	cp ../src/memfiles/* ./ 2>/dev/null || true
 	ghdl -a --std=08 --workdir=obj_dir ../src/static/multiplier.vhd ../src/static/mux.vhd ../src/static/negative.vhd ../src/static/shift_adder.vhd ../src/static/lookup_table.vhd $(wildcard ../src/$(VM_PREFIX:_wrapper=)_stage*.vhd) $(wildcard ../src/$(VM_PREFIX:_wrapper=).vhd) ../src/$(VM_PREFIX).vhd
 	ghdl synth --std=08 --workdir=obj_dir --out=verilog $(VM_PREFIX) > $(VM_PREFIX).v
 
